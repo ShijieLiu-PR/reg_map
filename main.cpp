83ec03ef2490455da12f4b9146e7a881d744c92c
Shijie Liu <shijie.liu@novosns.com>
2024-11-04 19:22:03 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "include/reg_map.h"
+
+
+int main(int argc, char* argv[])
+{
+    std::cout<< add(10,20)<<std::endl;
+    std::cout<<"Hello World"<<std::endl;
+    
+    return 0;
+}
